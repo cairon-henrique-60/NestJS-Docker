288b34bbd81e2c120ba49e25fe82fa1808a3e20d
cairon-henrique-60 <caironhenrique60@gmail.com>
2023-02-22 07:01:44 -0300

    Excluding DTO that is not in use

--- src/course/users/users.service.ts
+++ src/course/users/users.service.ts
@@ -5,14 +5,6 @@ import { FindManyOptions, Repository } from "typeorm";
 import { User } from "../entities/user.entity";
 import { CreateUsersDto } from "./dto/create-users.dto";
 
-class UserDto {
-    readonly findAll: () => {};
-    readonly findOne: (id: string) => {};
-    readonly create: (props: CreateUsersDto) => Promise<void>;
-    readonly update: (id: string, props: CreateUsersDto) => Promise<void>;
-    readonly deleteProps: (id: string) => Promise<void>;
-}
-
 @Injectable()
 export class UserService extends GeneralService<User> {
     constructor(
